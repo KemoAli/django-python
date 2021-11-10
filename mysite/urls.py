e28555e566dd71a15b0eb043f4acd3df3cefa1c5
Kemo Khan <saidyali88@gamil.com>
2021-11-10 09:36:02 -0500

    created models

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('mysite/', include('polls.urls')),
+    path('mysite/polls/', include('polls.urls')),
     path('admin/', admin.site.urls)
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse;
+from django.http import HttpResponse, response;
 
 # Create your views here.
 def index(req):
     return HttpResponse("Hello world!")
+
+def detail(req, question_id):
+    return HttpResponse('You are looking at question %s.' % question_id)
+
+def result(req, question_id):
+    resp = "Youre loooking at  result of question %s."
+    return HttpResponse(resp % question_id)
+
+def vote(req, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
